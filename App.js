42ee382442023f7b05c935ce5d85d4f3a6a6ff82
Andy Kim <hyungSeok.kim@outlook.com>
2020-04-01 13:16:21 +1300

    improve UI & state

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
-
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+import ToDo from './toDo'
 const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  }
+
+
   render() {
+    const { newToDo } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>Kawai To Do</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder="New To Do" ></TextInput>
+          <TextInput style={styles.input}
+            placeholder="New To Do"
+            value={newToDo}
+            onChangeText={this._controlNewToDo}
+            placeholderTextColor="#999"
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
         </View>
       </View>
     );
+  }
+  _controlNewToDo = text => {
+    this.setState({
+      newToDo: text
+    })
   }
 }
 
---
+++ toDo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+export default class toDo extends React.Component {
+    render() {
+        return <View>
+            <Text>Hello World</Text>
+        </View>
+    }
+
+}
+
+const styles = StyleSheet.create({
+
+})
